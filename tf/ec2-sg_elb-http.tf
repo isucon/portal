56e84ce573b4c134685311fe243accf8e1f1cb28
Sorah Fukumori <sorah@cookpad.com>
2020-07-08 07:32:41 +0900

    tf(ec2-sg_elb-http): revoke 80/tcp inbound

--- tf/ec2-sg_elb-http.tf
+++ tf/ec2-sg_elb-http.tf
@@ -8,15 +8,15 @@ resource "aws_security_group" "elb-http" {
   }
 }
 
-resource "aws_security_group_rule" "elb-http-ingress_http" {
-  security_group_id = aws_security_group.elb-http.id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 80
-  to_port           = 80
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-}
+#resource "aws_security_group_rule" "elb-http-ingress_http" {
+#  security_group_id = aws_security_group.elb-http.id
+#  type              = "ingress"
+#  protocol          = "tcp"
+#  from_port         = 80
+#  to_port           = 80
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  ipv6_cidr_blocks  = ["::/0"]
+#}
 
 resource "aws_security_group_rule" "elb-http-ingress_https" {
   security_group_id = aws_security_group.elb-http.id
