40ea7491745185eebe1b02e1634b91299a1ee125
Sorah Fukumori <sorah@cookpad.com>
2020-07-08 06:25:48 +0900

    api/audience: cc no-cache="Set-Cookie" and suppress session

--- portal/app/controllers/api/audience/application_controller.rb
+++ portal/app/controllers/api/audience/application_controller.rb
@@ -1,2 +1,8 @@
 class Api::Audience::ApplicationController < Api::ApplicationController
+  after_action :cc_no_cookie
+  
+  private def cc_no_cookie
+    request.session_options[:skip] = true
+    response.cache_control[:extras] << 'no-cache="Set-Cookie"'
+  end
 end
