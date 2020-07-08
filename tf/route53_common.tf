6059bedb511b687b1fc4e6616b43cefd403ca0f4
Sorah Fukumori <sorah@cookpad.com>
2020-07-08 16:13:26 +0900

    publish to portal.isucon.net

--- tf/route53_common.tf
+++ tf/route53_common.tf
@@ -123,3 +123,17 @@ resource "aws_route53_record" "cname_portal-dev-x-isucon-dev" {
   ttl     = "300"
   records = ["hako-isuxportal-dev-fargate-480405202.ap-northeast-1.elb.amazonaws.com."]
 }
+
+resource "aws_route53_record" "cname_portal-isucon-net-x-isucon-dev" {
+  for_each = {
+    for k in [
+      aws_route53_zone.public.zone_id,
+      aws_route53_zone.private.zone_id,
+    ] : k => k
+  }
+  zone_id = each.value
+  name    = "portal-isucon-net.x.isucon.dev"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_cloudfront_distribution.isuxportal-prd.domain_name}."]
+}
