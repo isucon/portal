29bcb3e091cd2dd69cf868abf95bda103cb125a8
Sorah Fukumori <sorah@cookpad.com>
2020-08-16 22:24:25 +0900

    AdminApp: aside nav menu

--- portal/app/javascript/AdminApp.tsx
+++ portal/app/javascript/AdminApp.tsx
@@ -8,6 +8,7 @@ import {
   Switch,
   Route,
   Link,
+  NavLink,
 } from "react-router-dom";
 
 import {ErrorMessage} from "./ErrorMessage";
@@ -39,34 +40,44 @@ export class AdminApp extends React.Component<Props, State> {
     return <BrowserRouter>
       <Navbar session={this.props.session} client={this.props.client} />
 
-      <div className="columns">
-        <div className="column is-3">
-          <aside className="menu">
-            <p className="menu-label">Aaaaa</p>
-          </aside>
-        </div>
+      <div className="container mt-5">
+        <div className="columns">
+          <div className="column is-3">
+            <aside className="menu">
+              <p className="menu-label">Contest</p>
+              <ul className="menu-list">
+                <li><NavLink exact to="/admin" activeClassName="is-active">Dashboard</NavLink></li>
+                <li><NavLink to="/admin/benchmark_jobs" activeClassName="is-active">Benchmark Jobs</NavLink></li>
+              </ul>
+              <p className="menu-label">Registration</p>
+              <ul className="menu-list">
+                <li><NavLink to="/admin/teams" activeClassName="is-active">Teams</NavLink></li>
+              </ul>
+            </aside>
+          </div>
 
-        <div className="column is-9">
-          <main>
-            <Switch>
-              <Route exact path="/admin" render={({match}) => {
-                return <></>;
+          <div className="column is-9">
+            <main>
+              <Switch>
+                <Route exact path="/admin" render={({match}) => {
+                  return <></>;
+                }} />
+                <Route exact path="/admin/teams" render={({match}) => {
+                  return <AdminTeamList session={this.props.session} client={this.state.adminClient} />;
+                }} />
+                <Route path="/admin/teams/:id" render={({match}) => {
+                  return <AdminTeamDetail session={this.props.session} client={this.state.adminClient} teamId={match.params.id} />;
               }} />
-              <Route exact path="/admin/teams" render={({match}) => {
-                return <AdminTeamList session={this.props.session} client={this.state.adminClient} />;
-              }} />
-              <Route path="/admin/teams/:id" render={({match}) => {
-                return <AdminTeamDetail session={this.props.session} client={this.state.adminClient} teamId={match.params.id} />;
-            }} />
-              <Route exact path="/admin/benchmark_jobs" render={({match, location}) => {
-                const query = new URLSearchParams(location.search);
-                return <AdminBenchmarkJobList session={this.props.session} client={this.state.adminClient} teamId={query.get("team_id")} incompleteOnly={query.get("incomplete_only") === '1'}/>;
-              }} />
-              <Route path="/admin/benchmark_jobs/:id" render={({match}) => {
-                return <AdminBenchmarkJobDetail session={this.props.session} client={this.state.adminClient} id={match.params.id} />;
-              }} />
-            </Switch>
-          </main>
+                <Route exact path="/admin/benchmark_jobs" render={({match, location}) => {
+                  const query = new URLSearchParams(location.search);
+                  return <AdminBenchmarkJobList session={this.props.session} client={this.state.adminClient} teamId={query.get("team_id")} incompleteOnly={query.get("incomplete_only") === '1'}/>;
+                }} />
+                <Route path="/admin/benchmark_jobs/:id" render={({match}) => {
+                  return <AdminBenchmarkJobDetail session={this.props.session} client={this.state.adminClient} id={match.params.id} />;
+                }} />
+              </Switch>
+            </main>
+          </div>
         </div>
       </div>
     </BrowserRouter>;
