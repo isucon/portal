859108454c889e2353054514248e4eabc43c4fe4
whywaita <whywaita@whywrite.it>
2020-08-28 16:10:20 +0900

    add record for private zone

--- tf/route53_common.tf
+++ tf/route53_common.tf
@@ -138,7 +138,7 @@ resource "aws_route53_record" "cname_portal-isucon-net-x-isucon-dev" {
   records = ["${aws_cloudfront_distribution.isuxportal-prd.domain_name}."]
 }
 
-resource "aws_route53_record" "a_cn-hv-x-isucon-dev" {
+resource "aws_route53_record" "a_cn-hv-x-isucon-dev-public" {
   count = 24
 
   zone_id = aws_route53_zone.public.zone_id
@@ -148,10 +148,31 @@ resource "aws_route53_record" "a_cn-hv-x-isucon-dev" {
   records = ["10.160.0.${140 + count.index + 1}"]
 }
 
-resource "aws_route53_record" "a_adm-hv-x-isucon-dev" {
+resource "aws_route53_record" "a_cn-hv-x-isucon-dev-private" {
+  count = 24
+
+  zone_id = aws_route53_zone.private.zone_id
+  name = "isucn${format("%04d", count.index + 1)}.hv.x.isucon.dev"
+  type = "A"
+  ttl = "300"
+  records = ["10.160.0.${140 + count.index + 1}"]
+}
+
+
+resource "aws_route53_record" "a_adm-hv-x-isucon-dev-public" {
   count = 8
 
   zone_id = aws_route53_zone.public.zone_id
+  name = "isuadm${format("%04d", count.index + 1)}.hv.x.isucon.dev"
+  type = "A"
+  ttl = "300"
+  records = ["10.160.0.${140 + 24 + count.index + 1}"]
+}
+
+resource "aws_route53_record" "a_adm-hv-x-isucon-dev-private" {
+  count = 8
+
+  zone_id = aws_route53_zone.private.zone_id
   name = "isuadm${format("%04d", count.index + 1)}.hv.x.isucon.dev"
   type = "A"
   ttl = "300"
