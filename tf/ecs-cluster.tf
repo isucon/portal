3feccd250eaffe22b0d82ec90b76eab89cbfd324
Sorah Fukumori <sorah@cookpad.com>
2020-08-28 21:38:17 +0900

    capacity_providers FARGATE_SPOT

--- tf/ecs-cluster.tf
+++ tf/ecs-cluster.tf
@@ -1,5 +1,6 @@
 resource "aws_ecs_cluster" "fargate" {
-  name = "isux-fargate"
+  name               = "isux-fargate"
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
   tags = {
     Project = "isucon10"
   }
