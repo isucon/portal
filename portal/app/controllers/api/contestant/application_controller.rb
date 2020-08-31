c5a95af82c1bdc4cdde3ecd2148a9ddb85f0132f
Sorah Fukumori <sorah@cookpad.com>
2020-09-01 06:59:55 +0900

    return 404 for contestant APIs until contest starts

--- portal/app/controllers/api/contestant/application_controller.rb
+++ portal/app/controllers/api/contestant/application_controller.rb
@@ -1,3 +1,10 @@
 class Api::Contestant::ApplicationController < Api::ApplicationController
   before_action :require_contestant
+  before_action :require_contest_started
+
+  private def require_contest_started
+    if !Contest.contest_end? && !Contest.contest_running?
+      raise Api::ApplicationController::Error::NotFound.new("not yet started")
+    end
+  end
 end
