0a91c5f3a19c6729ff57f9a072365f1d5efbde14
Sorah Fukumori <sorah@cookpad.com>
2020-09-01 15:07:49 +0900

    sync_all_ssh_keys: handle 429

--- portal/app/jobs/sync_ssh_keys_of_all_contestants_job.rb
+++ portal/app/jobs/sync_ssh_keys_of_all_contestants_job.rb
@@ -4,6 +4,14 @@ class SyncSshKeysOfAllContestantsJob < ApplicationJob
     Contestant.find_in_batches do |batch|
       batch.each do |c|
         SyncSshKeysOfContestantJob.perform_now(c, access_token)
+        sleep 2
+      rescue Octokit::NotFound => e
+        Rails.logger.warn "#{e.inspect}"
+        Raven.capture_exception(e, level: :warning)
+      rescue Octokit::TooManyRequests => e
+        Rails.logger.warn "#{e.inspect}"
+        sleep 30
+        retry
       end
     end
   end
