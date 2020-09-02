6dad228c9eb2305170a80fc092213d9c95e50d58
Sorah Fukumori <sorah@cookpad.com>
2020-09-02 15:38:47 +0900

    contestant/dashboards: remove jobs
    follow-up: https://github.com/isucon/isucon10-portal/pull/16

--- portal/app/controllers/api/contestant/dashboards_controller.rb
+++ portal/app/controllers/api/contestant/dashboards_controller.rb
@@ -6,9 +6,6 @@ class Api::Contestant::DashboardsController < Api::Contestant::ApplicationContro
     render protobuf: Isuxportal::Proto::Services::Contestant::DashboardResponse.new(
       leaderboard: Contest.leaderboard(admin: false, team: current_team),
       instances: [], # TODO: ContestantInstance,
-      jobs: BenchmarkJob.where(team: current_team).order(id: :desc).joins_score.limit(5).map do |job|
-        job.to_pb(admin: false, team: false)
-      end,
     )
   end
 end
