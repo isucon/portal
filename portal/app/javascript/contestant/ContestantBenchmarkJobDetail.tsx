b1b8b71e14ed7ebdba8eb79d5826d1472dbcfbd4
Sorah Fukumori <sorah@cookpad.com>
2020-09-07 02:39:47 +0900

    ReloadButton and auto reload on *BenchmarkJobDetail

--- portal/app/javascript/contestant/ContestantBenchmarkJobDetail.tsx
+++ portal/app/javascript/contestant/ContestantBenchmarkJobDetail.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import {BenchmarkJobDetail} from "../BenchmarkJobDetail";
 
 import {ErrorMessage} from "../ErrorMessage";
+import {ReloadButton} from "../ReloadButton";
 
 export interface Props {
   session: isuxportal.proto.services.common.GetCurrentSessionResponse,
@@ -17,6 +18,7 @@ export interface State {
   job: isuxportal.proto.resources.IBenchmarkJob | null,
   error: Error | null,
   requesting: boolean,
+  timer: number | null,
 }
 
 export class ContestantBenchmarkJobDetail extends React.Component<Props, State> {
@@ -26,30 +28,45 @@ export class ContestantBenchmarkJobDetail extends React.Component<Props, State> 
       job: null,
       error: null,
       requesting: false,
+      timer: null,
     };
   }
 
   public componentDidMount() {
     this.updateJob();
+    this.setState({timer: window.setInterval(this.updateJob.bind(this), 5000)});
   }
 
   public componentDidUpdate(prevProps: Props, prevState: State) {
     if (prevProps !== this.props) this.updateJob();
   }
 
+  public componentWillUnmount() {
+    if (this.state.timer) window.clearInterval(this.state.timer);
+  }
+
   async updateJob() {
+    if (this.state.requesting) return;
     try {
+      this.setState({requesting: true});
       const resp = await this.props.client.getBenchmarkJob(this.props.id);
-      this.setState({job: resp.job!});
+      this.setState({job: resp.job!, requesting: false, error: null});
     } catch (error) {
-      this.setState({error});
+      this.setState({error, requesting: false});
     }
   }
 
   public render() {
     return <>
       <header>
-        <h1 className="title is-1">Job #{this.props.id}</h1>
+        <div className="level">
+          <div className="level-left">
+            <h1 className="title is-1">Job #{this.props.id}</h1>
+          </div>
+          <div className="level-right">
+            <ReloadButton requesting={this.state.requesting} onClick={this.updateJob.bind(this)} />
+          </div>
+        </div>
       </header>
       <main>
         {this.renderError()}
