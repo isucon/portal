c48caf943000e1f385a542d7ab1e29c4aa4fc9a7
Sorah Fukumori <sorah@cookpad.com>
2020-09-07 07:42:01 +0900

    SyncSshKeysOfAllContestantsJob: more logs

--- portal/app/jobs/sync_ssh_keys_of_all_contestants_job.rb
+++ portal/app/jobs/sync_ssh_keys_of_all_contestants_job.rb
@@ -1,12 +1,16 @@
 class SyncSshKeysOfAllContestantsJob < ApplicationJob
   self.log_arguments = false
   def perform(access_token)
+    Rails.logger.info "Syncing ssh public keys of ALL contestants"
     Contestant.order(id: :desc).includes(:ssh_public_keys).find_in_batches do |batch|
       without_keys, with_keys = batch.partition{ |_| _.ssh_public_keys.empty? }
-      without_keys.each do |c|
+      Rails.logger.info "Syncing ssh public keys of ALL contestants: batch (without=#{without_keys.size}, with=#{with_keys.size})"
+      without_keys.each_with_index do |c,idx|
+        Rails.logger.info "Syncing ssh public keys of ALL contestants: batch without_keys (#{idx+1}/#{without_keys.size}) contestant_id=#{c.id} contestant.github_login=#{c.github_login} contestant.name=#{c.name}"
         perform_contestant(c, access_token)
       end
-      with_keys.each do |c|
+      with_keys.each_with_index do |c,idx|
+        Rails.logger.info "Syncing ssh public keys of ALL contestants: batch with_keys (#{idx+1}/#{with_keys.size}) contestant_id=#{c.id} contestant.github_login=#{c.github_login} contestant.name=#{c.name}"
         perform_contestant(c, access_token)
       end
     end
