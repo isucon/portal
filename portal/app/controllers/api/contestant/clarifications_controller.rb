84a8ec0689b667d1e003486f0889b7326bb92268
Sorah Fukumori <sorah@cookpad.com>
2020-09-07 02:46:33 +0900

    Contestant/ListClarifications: order by created_at desc

--- portal/app/controllers/api/contestant/clarifications_controller.rb
+++ portal/app/controllers/api/contestant/clarifications_controller.rb
@@ -3,7 +3,7 @@ require 'isuxportal/services/contestant/clarifications_pb'
 class Api::Contestant::ClarificationsController < Api::Contestant::ApplicationController
   pb :index, Isuxportal::Proto::Services::Contestant::ListClarificationsQuery
   def index
-    @clarifications = Clarification.includes(:team).visible_for_team(current_team).order(answered_at: :desc)
+    @clarifications = Clarification.includes(:team).visible_for_team(current_team).order(created_at: :desc)
 
     render protobuf: Isuxportal::Proto::Services::Contestant::ListClarificationsResponse.new(
       clarifications: @clarifications.map do |clarification|
