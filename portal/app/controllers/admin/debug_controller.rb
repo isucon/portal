83a6e029043ef1403b27cb06099dc98f17fd8766
Sorah Fukumori <sorah@cookpad.com>
2020-09-07 05:12:23 +0900

    debug#ssh_key_stats: show github login

--- portal/app/controllers/admin/debug_controller.rb
+++ portal/app/controllers/admin/debug_controller.rb
@@ -14,7 +14,7 @@ class Admin::DebugController < Admin::ApplicationController
     Team.active.includes(members: :ssh_public_keys).find_in_batches do |batch|
       batch.each do |team|
         not_present_all = team.members.all? { |contestant| contestant.ssh_public_keys.empty? }
-        members_wo_ssh_keys = team.members.select{ |c|  c.ssh_public_keys.empty? }.map { |c| "#{c.name} (##{c.id}, #{c.discord_tag})" }
+        members_wo_ssh_keys = team.members.select{ |c|  c.ssh_public_keys.empty? }.map { |c| "#{c.name} (##{c.id}, #{c.github_login}/#{c.github_id}, #{c.discord_tag})" }
         unless members_wo_ssh_keys.empty?
           result << "#{team.name} (##{team.id}): #{not_present_all ? '*全員無登録*' : ''} #{members_wo_ssh_keys.join(', ')}"
         end
