28581f7b5b3f6f3ebfa718333b9db18b7dba3076
Sorah Fukumori <sorah@cookpad.com>
2020-09-09 04:16:42 +0900

    disable x-pb-inspect (can't do well with large objects)

--- portal/config/initializers/protobuf.rb
+++ portal/config/initializers/protobuf.rb
@@ -3,7 +3,7 @@ require 'isuxportal/error_pb'
 Mime::Type.register "application/vnd.google.protobuf", :protobuf
 ActionController::Renderers.add :protobuf do |obj, options|
   unless Rails.env.production?
-    response.headers['X-Pb-Inspect'] = obj.inspect
+    #response.headers['X-Pb-Inspect'] = obj.inspect
   end
   send_data obj.class.encode(obj), type: "application/vnd.google.protobuf; proto=#{obj.class.descriptor.name}"
 end
