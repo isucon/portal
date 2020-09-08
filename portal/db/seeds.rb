26e6b1343898844fa5a4526da920a5cb71d3f62a
Issei Naruta <naruta@cookpad.com>
2020-09-08 23:17:15 +0900

    Start implementing db:seeds for debugging

--- portal/db/seeds.rb
+++ portal/db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+def generate_team_and_members(student: false)
+  contestants = 3.times.map do |i|
+    login = Faker::Internet.unique.username
+    Contestant.new(
+      name: Faker::Name.unique.name,
+      github_login: login,
+      github_id: login,
+      discord_tag: login,
+      discord_id: login,
+      avatar_url: 'https://avatars1.githubusercontent.com/u/230654?s=400&v=4',
+      student: student,
+    )
+  end
+
+  team = Team.new(
+    name: Faker::Team.unique.name,
+    email_address: Faker::Internet.unique.email,
+    student: student,
+  )
+  team.members = contestants
+  contestants.map(&:save!)
+  team.leader_id = contestants.first.id
+  team.save!
+  team
+end
+
+teams = []
+200.times do
+  teams << generate_team_and_members(student: false)
+end
+100.times do
+  teams << generate_team_and_members(student: true)
+end
