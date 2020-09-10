ba29008c77028bef427147c7f62b6949cbdd25fc
Sorah Fukumori <sorah@cookpad.com>
2020-09-11 00:37:26 +0900

    AdminClarificationDetail: templates

--- portal/app/javascript/admin/AdminClarificationDetail.tsx
+++ portal/app/javascript/admin/AdminClarificationDetail.tsx
@@ -9,6 +9,24 @@ import { useForm } from "react-hook-form";
 import { Clarification } from "../Clarification";
 import { ErrorMessage } from "../ErrorMessage";
 
+interface QuickButtonProps {
+  caption?: string,
+  id?: string,
+  body: string,
+  onClick: (body: string) => any,
+}
+
+const ClarQuickButton: React.FC<QuickButtonProps> = (props: QuickButtonProps) => {
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    props.onClick(props.body);
+  }
+  return <button className="button" onClick={onClick}>
+    {props.id ? <span>({props.id}) </span> : null} {props.caption ?? props.body.slice(0,25)}
+  </button>
+};
+
+
 interface FormProps {
   session: isuxportal.proto.services.common.GetCurrentSessionResponse;
   client: AdminApiClient;
@@ -32,6 +50,14 @@ const ClarForm: React.FC<FormProps> = (props: FormProps) => {
     },
   });
 
+  const [list, setList] = React.useState<isuxportal.proto.resources.IClarification[] | null>(null);
+  React.useEffect(() => {
+    props.client
+      .listClarifications()
+      .then((resp) => setList(resp.clarifications))
+      .catch((e) => setError(e));
+  }, []);
+
   const onSubmit = handleSubmit(async (data, e) => {
     try {
       setRequesting(true);
@@ -51,6 +77,10 @@ const ClarForm: React.FC<FormProps> = (props: FormProps) => {
     }
   });
 
+  const onQuickButton = (body: string) => {
+    setValue('answer', body, { shouldDirty: true });
+  }
+
   return (
     <div className="card mt-5">
       <div className="card-header">
@@ -59,51 +89,76 @@ const ClarForm: React.FC<FormProps> = (props: FormProps) => {
       <div className="card-content">
         <form onSubmit={onSubmit}>
           <div className="columns">
-            <div className="column field is-6">
-              <label className="label" htmlFor="AdminClarificationDetail-question">
-                Question
-              </label>
-              <div className="control">
-                <textarea
-                  className="textarea"
-                  name="question"
-                  id="AdminClarificationDetail-question"
-                  ref={register}
-                  placeholder=""
-                  autoFocus
-                />
-              </div>
-            </div>
-            <div className="column field is-6">
-              <div className="control">
-                <label className="label" htmlFor="AdminClarificationDetail-answer">
-                  Answer
+            <div className="column is-6">
+              <div className="field">
+                <label className="label" htmlFor="AdminClarificationDetail-question">
+                  Question
                 </label>
                 <div className="control">
                   <textarea
                     className="textarea"
-                    name="answer"
-                    id="AdminClarificationDetail-answer"
+                    name="question"
+                    id="AdminClarificationDetail-question"
                     ref={register}
                     placeholder=""
                   />
                 </div>
               </div>
+
+              <div className="field">
+                <div className="control">
+                  <label className="checkbox">
+                    <input type="checkbox" name="disclose" ref={register} />
+                    Disclose (Visible to all teams)
+                  </label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <button className="button is-primary" type="submit" disabled={requesting}>
+                    Respond
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <label className="checkbox">
-                <input type="checkbox" name="disclose" ref={register} />
-                Disclose (Visible to all teams)
-              </label>
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <button className="button is-primary" type="submit" disabled={requesting}>
-                Respond
-              </button>
+
+            <div className="column is-6">
+              <div className="field">
+                <div className="control">
+                  <label className="label" htmlFor="AdminClarificationDetail-answer">
+                    Answer
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name="answer"
+                      id="AdminClarificationDetail-answer"
+                      ref={register}
+                      placeholder=""
+                      autoFocus
+                    />
+                  </div>
+                </div>
+
+                <section className="mt-5">
+                  <h5 className="is-5 subtitle">Quick Response</h5>
+                  <div className="buttons">
+                    <ClarQuickButton body="はい" caption="Yes" onClick={onQuickButton} />
+                    <ClarQuickButton body="いいえ" caption="No" onClick={onQuickButton} />
+                    <ClarQuickButton body="ドキュメント (マニュアル、レギュレーション、ルール等) や既存のアナウンスに記載されています" caption="Answered in task description" onClick={onQuickButton} />
+                    <ClarQuickButton body="質問が不明瞭のため回答できません" caption="Invalid question" onClick={onQuickButton} />
+                    <ClarQuickButton body="その内容には回答できません" caption="No comment" onClick={onQuickButton} />
+                  </div>
+                </section>
+
+                <section className="mt-5">
+                  <h5 className="is-5 subtitle">Past Responses</h5>
+                  {list ? <div className="buttons are-small">
+                    {list.filter((v) => v.answered && v.question !== "Discord Support Channel Log")
+                      .map((v) => <ClarQuickButton id={v.id!.toString()} body={`${v.disclosed ? `既に Clarification #${v.id!} で回答されている内容です(再掲します)\n\n` : ""}${v.answer!}`} caption={v.answer!.slice(0,25)} onClick={onQuickButton} key={v.id!.toString()} />)}
+                  </div> : <p>Loading</p>}
+                </section>
+              </div>
             </div>
           </div>
           {error ? <ErrorMessage error={error} /> : null}
