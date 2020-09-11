cfb2d44590ecc59af94d19c596da7b7e8f247a10
Sorah Fukumori <sorah@cookpad.com>
2020-09-11 21:28:34 +0900

    SyncSshKeysOfAllContestantsJob: no_key_only

--- portal/app/jobs/sync_ssh_keys_of_all_contestants_job.rb
+++ portal/app/jobs/sync_ssh_keys_of_all_contestants_job.rb
@@ -1,6 +1,6 @@
 class SyncSshKeysOfAllContestantsJob < ApplicationJob
   self.log_arguments = false
-  def perform(access_token)
+  def perform(access_token, no_key_only: false)
     Rails.logger.info "Syncing ssh public keys of ALL contestants"
     Contestant.order(id: :desc).includes(:ssh_public_keys).find_in_batches do |batch|
       without_keys, with_keys = batch.partition{ |_| _.ssh_public_keys.empty? }
@@ -9,9 +9,11 @@ class SyncSshKeysOfAllContestantsJob < ApplicationJob
         Rails.logger.info "Syncing ssh public keys of ALL contestants: batch without_keys (#{idx+1}/#{without_keys.size}) contestant_id=#{c.id} contestant.github_login=#{c.github_login} contestant.name=#{c.name}"
         perform_contestant(c, access_token)
       end
-      with_keys.each_with_index do |c,idx|
-        Rails.logger.info "Syncing ssh public keys of ALL contestants: batch with_keys (#{idx+1}/#{with_keys.size}) contestant_id=#{c.id} contestant.github_login=#{c.github_login} contestant.name=#{c.name}"
-        perform_contestant(c, access_token)
+      unless no_key_only
+        with_keys.each_with_index do |c,idx|
+          Rails.logger.info "Syncing ssh public keys of ALL contestants: batch with_keys (#{idx+1}/#{with_keys.size}) contestant_id=#{c.id} contestant.github_login=#{c.github_login} contestant.name=#{c.name}"
+          perform_contestant(c, access_token)
+        end
       end
     end
   end
