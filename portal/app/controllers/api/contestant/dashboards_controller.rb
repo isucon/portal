df4f3f2e461964350acd940cd535669cc1895207
Sorah Fukumori <sorah@cookpad.com>
2020-09-12 20:00:40 +0900

    etag

--- portal/app/controllers/api/contestant/dashboards_controller.rb
+++ portal/app/controllers/api/contestant/dashboards_controller.rb
@@ -4,13 +4,11 @@ class Api::Contestant::DashboardsController < Api::Contestant::ApplicationContro
   pb :show, Isuxportal::Proto::Services::Contestant::DashboardQuery
   def show
     expires_in 0, public: false, must_revalidate: true
-    if stale?(etag: Contest.leaderboard_etag_cached(admin: false, team: current_team))
       resp = Rails.cache.fetch("contestantdashboard3-t#{current_team.id}", expires_in: 12.hours) do
         Isuxportal::Proto::Services::Contestant::DashboardResponse.encode(Isuxportal::Proto::Services::Contestant::DashboardResponse.new(
           leaderboard: Contest.leaderboard(admin: false, team: current_team), # TODO: disable progresses
         ))
       end
       render protobuf: resp
-    end
   end
 end
