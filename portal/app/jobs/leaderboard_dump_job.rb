eee8dae065f5bee86e633895f1293ad9b1f0670e
Sorah Fukumori <sorah@cookpad.com>
2020-09-12 23:17:31 +0900

    target id

--- portal/app/jobs/leaderboard_dump_job.rb
+++ portal/app/jobs/leaderboard_dump_job.rb
@@ -1,6 +1,13 @@
 class LeaderboardDumpJob < ApplicationJob
   def perform(now)
     lb = Contest.leaderboard(admin: true, team: nil, progresses: false, solo: false, now: now)
+
+    targets = {}
+    lb.teams.each do |item|
+      job = BenchmarkJob.where(team_id: item.team.id).order(id: :desc).limit(1).first
+      next unless job
+      targets[item.team.id] = job.target
+    end
     lb.teams.each_with_index do |item,idx|
       log_data(
         position: idx+1,
@@ -9,7 +16,9 @@ class LeaderboardDumpJob < ApplicationJob
         student: item.team.student.status,
         best_score: item.best_score&.score,
         latest_score: item.latest_score&.score,
-        latest_score_at: item.latest_score&.marked_at&.seconds,
+        latest_score_at: item.latest_score&.marked_at&.seconds&.yield_self{ |ts| Time.at(ts).xmlschema },
+        target: targets[item.team.id]&.id,
+        target_ip: targets[item.team.id]&.public_ipv4_address,
       )
     end
     nil
