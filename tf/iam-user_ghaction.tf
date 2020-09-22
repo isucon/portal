942a42daa4a0e67597d8261e590dd95521b399ef
Sorah Fukumori <sorah@cookpad.com>
2020-09-22 23:03:55 +0900

    supervisor: Upload binaries to S3

--- tf/iam-user_ghaction.tf
+++ tf/iam-user_ghaction.tf
@@ -31,3 +31,22 @@ data "aws_iam_policy_document" "ghaction-ecr" {
   }
 }
 
+
+resource "aws_iam_user_policy" "ghaction-s3" {
+  user   = aws_iam_user.ghaction.name
+  name   = "s3"
+  policy = data.aws_iam_policy_document.ghaction-s3.json
+}
+
+data "aws_iam_policy_document" "ghaction-s3" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.isucon10-machine-images.arn}/supervisor/*",
+    ]
+  }
+}
+
