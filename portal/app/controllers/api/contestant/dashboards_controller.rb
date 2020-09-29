3edff89457fbc44b68e245947ad938642b9a0dc4
Sorah Fukumori <sorah@cookpad.com>
2020-09-29 12:24:18 +0900

    contestant: default to unfrozen cache

--- portal/app/controllers/api/contestant/dashboards_controller.rb
+++ portal/app/controllers/api/contestant/dashboards_controller.rb
@@ -4,7 +4,7 @@ class Api::Contestant::DashboardsController < Api::Contestant::ApplicationContro
   pb :show, Isuxportal::Proto::Services::Contestant::DashboardQuery
   def show
     # See also: UpdateContestantDashboardJob
-    ptr = Rails.cache.read("contestantdashboard4-t#{current_team.id}-pointer")
+    ptr = Rails.cache.read("contestantdashboard4-t#{current_team.id}-pointer") || 'contestantdashboard4-unfrozen'
     if ptr && ptr.start_with?("contestantdashboard4-")
       ptr_resp = Rails.cache.read(ptr)
       if ptr_resp
