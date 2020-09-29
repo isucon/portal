28b533a86e194c664526ef57b4084ae8a8384246
Sorah Fukumori <sorah@cookpad.com>
2020-09-29 12:14:40 +0900

    Use application/protobuf for CloudFront

--- portal/config/initializers/protobuf.rb
+++ portal/config/initializers/protobuf.rb
@@ -1,17 +1,18 @@
 # frozen_string_literals: true
 require 'isuxportal/error_pb'
 
-Mime::Type.register 'application/vnd.google.protobuf', :protobuf
+# https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/ServingCompressedFiles.html#compressed-content-cloudfront-file-types
+Mime::Type.register 'application/protobuf', :protobuf
 ActionController::Renderers.add :protobuf do |obj, options|
   #unless Rails.env.production?
     #response.headers['X-Pb-Inspect'] = obj.inspect
   #end
   if obj.is_a?(String)
-    self.content_type =    "application/vnd.google.protobuf"
+    self.content_type =    "application/protobuf"
     response.charset = false
     obj
   else
-    self.content_type =    "application/vnd.google.protobuf; proto=#{obj.class.descriptor.name}"
+    self.content_type =    "application/protobuf; proto=#{obj.class.descriptor.name}"
     response.charset = false
     obj.class.encode(obj)
   end
