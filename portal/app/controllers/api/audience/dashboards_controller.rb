4acc18a51e31dff60dd7e77c782555f8b2678df6
Sorah Fukumori <sorah@cookpad.com>
2020-09-29 12:25:37 +0900

    audience/dashboard: cache-control must_revalidate=false

--- portal/app/controllers/api/audience/dashboards_controller.rb
+++ portal/app/controllers/api/audience/dashboards_controller.rb
@@ -3,7 +3,7 @@ require 'isuxportal/services/audience/dashboard_pb'
 class Api::Audience::DashboardsController < Api::Audience::ApplicationController
   pb :show, Isuxportal::Proto::Services::Audience::DashboardQuery
   def show
-    expires_in 5.seconds, public: true, 's-maxage' => '10', must_revalidate: true
+    expires_in 5.seconds, public: true, 's-maxage' => '10'
     render protobuf: Rails.cache.read('audiencedashboard4-unfrozen') || Isuxportal::Proto::Services::Audience::DashboardResponse.new(
       leaderboard: Contest.leaderboard(admin: false, team: nil), # TODO: disable progresses
     )
