e1e8e0b43fede470d0629a0c86ef40cb5363cb2a
Sorah Fukumori <sorah@cookpad.com>
2020-10-03 13:12:31 +0900

    scale up AWS resources for isucon10-final

--- tf/ec2-sg_redis.tf
+++ tf/ec2-sg_redis.tf
@@ -24,6 +24,14 @@ resource "aws_security_group_rule" "redis-ingress_redis_bastion" {
   to_port                  = 6379
   source_security_group_id = aws_security_group.bastion.id
 }
+resource "aws_security_group_rule" "redis-ingress_redis_self" {
+  security_group_id        = aws_security_group.redis.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 6379
+  to_port                  = 6379
+  source_security_group_id = aws_security_group.redis.id
+}
 
 resource "aws_security_group_rule" "redis-egress_all" {
   security_group_id = aws_security_group.redis.id
