dc68a1352e3cdf685e88a8e1f4c69b234cb82c1b
yfujit <yfujita813@gmail.com>
2021-06-10 13:04:49 +0900

    fix resource name for dev

--- tf/sqs.tf
+++ tf/sqs.tf
@@ -1,12 +1,12 @@
-resource "aws_sqs_queue" "isuxportal-activejob-dlq" {
-  name = "isuxportal-activejob-dlq"
+resource "aws_sqs_queue" "isuxportal-activejob-dlq-dev" {
+  name = "isuxportal-activejob-dlq-dev"
 }
 
-resource "aws_sqs_queue" "isuxportal-activejob" {
-  name = "isuxportal-activejob"
+resource "aws_sqs_queue" "isuxportal-activejob-dev" {
+  name = "isuxportal-activejob-dev"
 
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.isuxportal-activejob-dlq.arn
+    deadLetterTargetArn = aws_sqs_queue.isuxportal-activejob-dlq-dev.arn
     maxReceiveCount     = 10
   })
 }
