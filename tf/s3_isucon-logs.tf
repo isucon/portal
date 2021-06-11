e6119ad96025f618ea37d3e822a98a457aa5291c
yfujit <yfujita813@gmail.com>
2021-06-11 15:39:55 +0900

    add s3 buckets for logs and public

---
+++ tf/s3_isucon-logs.tf
@@ -0,0 +1,34 @@
+variable aws_account_id {}
+variable aws_account_canonical_id {}
+
+resource "aws_s3_bucket" "isucon-logs" {
+  bucket = "isucon-logs"
+
+  grant {
+    id          = "var.aws_account_canonical_id"
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "isucon-logs" {
+  bucket = "isucon-logs"
+  policy = data.aws_iam_policy_document.allow-putting-elb-access-logs.json
+}
+
+data "aws_iam_policy_document" "allow-putting-elb-access-logs" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      // Elastic Load Balancing Account ID of ap-northeast-1
+      // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions
+      identifiers = [var.aws_account_id]
+    }
+    actions = ["s3:PutObject"]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.isucon-logs.id}/*",
+    ]
+  }
+}
+
---
+++ tf/s3_isucon-public.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "isucon-public" {
+  bucket = "isucon-public"
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket_policy" "isucon-public" {
+  bucket = aws_s3_bucket.isucon-public.bucket
+  policy = data.aws_iam_policy_document.isucon-public.json
+}
+
+data "aws_iam_policy_document" "isucon-public" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = ["s3:GetObject"]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.isucon-public}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = ["s3:ListBucket"]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.isucon-public}",
+    ]
+    condition {
+      test     = "StringLike"
+      variable = "s3:Prefix"
+      values   = ["git/6MGgQp9lDmsK6G0CNa0DCOWHkOXID9-1davWxzKzI28/*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
+}
