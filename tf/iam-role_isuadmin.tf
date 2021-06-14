038ab01c625d9fd14f45dd6989bf7768bfda0af5
Sorah Fukumori <sorah@cookpad.com>
2021-06-15 07:58:16 +0900

    IsuAdmin: fix boundary bugs
    - Put*PermissionsBoundary was not allowed with IsuAdmin
    - Explicit deny to IsuAdmin and Admin role/policies weren't properly
      enforced; instead, as NotResources was in use, almost all write
      access to IAM resources were rejected.

--- tf/iam-role_isuadmin.tf
+++ tf/iam-role_isuadmin.tf
@@ -52,6 +52,7 @@ data "aws_iam_policy_document" "isuadmin" {
   }
 
   statement {
+    effect = "Allow"
     actions = [
       "iam:AttachRolePolicy",
       "iam:CreateRole",
@@ -63,6 +64,9 @@ data "aws_iam_policy_document" "isuadmin" {
       "iam:PutUserPolicy",
       "iam:AttachUserPolicy",
       "iam:DeleteUserPolicy",
+
+      "iam:PutRolePermissionsBoundary",
+      "iam:PutUserPermissionsBoundary",
     ]
 
     resources = ["*"]
@@ -84,9 +88,11 @@ data "aws_iam_policy_document" "isuadmin" {
       "iam:Detach*",
       "iam:Attach*",
       "iam:Create*",
+      "iam:Put*",
+      "iam:Remove*",
     ]
 
-    not_resources = [
+    resources = [
       aws_iam_role.IsuAdmin.arn,
       "arn:aws:iam::245943874622:role/Admin",
       "arn:aws:iam::245943874622:policy/IsuAdmin", # Cannot use aws_iam_policy.*.arn because of cyclic
