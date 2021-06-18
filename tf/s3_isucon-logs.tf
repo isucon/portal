940e01bb03532118d220713f7ecf8f8822b0cbe5
yfujit <yfujita813@gmail.com>
2021-06-18 14:44:25 +0900

    delete tfvars params

--- tf/s3_isucon-logs.tf
+++ tf/s3_isucon-logs.tf
@@ -1,11 +1,8 @@
-variable aws_account_id {}
-variable aws_account_canonical_id {}
-
 resource "aws_s3_bucket" "isucon-logs" {
   bucket = "isucon-logs"
 
   grant {
-   id          = var.aws_account_canonical_id
+   id          = "809c053be6795100a6fb06810c8a3ddf944f086e209cddcf82102fc3174e869b"
    type        = "CanonicalUser"
    permissions = ["FULL_CONTROL"]
  }
@@ -23,7 +20,7 @@ data "aws_iam_policy_document" "allow-putting-elb-access-logs" {
       type = "AWS"
       // Elastic Load Balancing Account ID of ap-northeast-1
       // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions
-      identifiers = [var.aws_account_id]
+      identifiers = ["245943874622"]
     }
     actions = ["s3:PutObject"]
     resources = [
