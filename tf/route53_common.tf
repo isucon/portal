f2d2d67e61b976028ece76faa67c85461af2664c
yfujit <yfujita813@gmail.com>
2021-06-24 02:49:52 +0900

    add cloudfront and route53 for portal prd

--- tf/route53_common.tf
+++ tf/route53_common.tf
@@ -1,26 +1,42 @@
-#resource "aws_route53_record" "cname_portal-prd-xi-isucon-dev" {
-#  zone_id = aws_route53_zone.public.zone_id
-#  name    = "portal-prd.xi.isucon.dev"
-#  type    = "CNAME"
-#  ttl     = "300"
-#  records = ["tmp"]
-#}
-#
-#resource "aws_route53_record" "cname_portal-grpc-prd-x-isucon-dev" {
-#  zone_id = aws_route53_zone.public.zone_id
-#  name    = "portal-grpc-prd.xi.isucon.dev"
-#  type    = "CNAME"
-#  ttl     = "300"
-#  records = ["tmp"]
-#}
-#
-#resource "aws_route53_record" "cname_portal-isucon-net-x-isucon-dev" {
-#  zone_id = aws_route53_zone.public.zone_id
-#  name    = "portal-isucon-net.xi.isucon.dev"
-#  type    = "CNAME"
-#  ttl     = "300"
-#  records = ["tmp"]
-#}
+########################################
+# Production
+########################################
+
+data "aws_lb" "hako-isuxportal-prd-fargate" {
+  name = "hako-isuxportal-prd-fargate"
+}
+
+resource "aws_route53_record" "cname_portal-prd-xi-isucon-dev" {
+  for_each = {
+    for k in [
+      aws_route53_zone.public.zone_id,
+      #aws_route53_zone.private.zone_id,
+    ] : k => k
+  }
+  zone_id = each.value
+  name    = "portal-prd.xi.isucon.dev"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [data.aws_lb.hako-isuxportal-prd-fargate.dns_name]
+}
+
+resource "aws_route53_record" "cname_portal-prd-isucon-net-xi-isucon-dev" {
+  for_each = {
+    for k in [
+      aws_route53_zone.public.zone_id,
+      #aws_route53_zone.private.zone_id,
+    ] : k => k
+  }
+  zone_id = each.value
+  name    = "portal-prd-isucon-net.xi.isucon.dev"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_cloudfront_distribution.isuxportal-prd.domain_name}."]
+}
+########################################
+# Dev
+########################################
+
 
 data "aws_lb" "hako-isuxportal-dev-fargate" {
   name = "hako-isuxportal-dev-fargate"
