6d1cfa97af1435091ebedfd87d02d92e14821e45
sapphi-red <green@sapphi.red>
2021-07-07 20:11:00 +0900

    protobuf生成スクリプトの更新

--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,9 @@
-#!/bin/zsh -xe
-cd "$(dirname $0)"
+#!/bin/bash -xe
 (
+  cd "$(dirname $0)"
+
+  shopt -s globstar
+
   mkdir -p tmp/protoc-go
   protoc -I=proto --go_out=tmp/protoc-go --go-grpc_out=tmp/protoc-go proto/**/*.proto
   rm -rf proto.go
@@ -11,26 +14,28 @@ cd "$(dirname $0)"
 (
   cd "$(dirname $0)/portal"
 
+  shopt -s globstar
+
   rm -rf app/javascript/pb*.js  app/javascript/pb*.d.ts lib/isuxportal sw/src/pb.js sw/src/pb*.d.ts || :
   bundle exec grpc_tools_ruby_protoc -I../proto --ruby_out=./lib --grpc_out=./lib ../proto/**/*.proto
 
-  npx pbjs -p ../proto -t static-module -w commonjs -o app/javascript/pb.js \
+  npx -p protobufjs -c "pbjs -p ../proto -t static-module -w commonjs -o app/javascript/pb.js \
     ../proto/google/**/*.proto \
     ../proto/isuxportal/*.proto \
     ../proto/isuxportal/resources/*.proto \
-    ../proto/isuxportal/services/{common,audience,contestant,registration}/*.proto 
-  npx pbts -o app/javascript/pb.d.ts app/javascript/pb.js
+    ../proto/isuxportal/services/{common,audience,contestant,registration}/*.proto"
+  npx -p protobufjs -c "pbts -o app/javascript/pb.d.ts app/javascript/pb.js"
 
-  npx pbjs -p ../proto -t static-module -w commonjs -o app/javascript/pb_admin.js \
+  npx -p protobufjs -c "pbjs -p ../proto -t static-module -w commonjs -o app/javascript/pb_admin.js \
     ../proto/google/**/*.proto \
     ../proto/isuxportal/*.proto \
     ../proto/isuxportal/resources/*.proto \
-    ../proto/isuxportal/services/{common,admin,audience,contestant,registration}/*.proto 
-  npx pbts -o app/javascript/pb_admin.d.ts app/javascript/pb_admin.js
+    ../proto/isuxportal/services/{common,admin,audience,contestant,registration}/*.proto"
+  npx -p protobufjs -c "pbts -o app/javascript/pb_admin.d.ts app/javascript/pb_admin.js"
 
-  npx pbjs -p ../proto -t static-module -w commonjs -o sw/src/pb.js \
+  npx -p protobufjs -c "pbjs -p ../proto -t static-module -w commonjs -o sw/src/pb.js \
     ../proto/google/**/*.proto \
-    ../proto/isuxportal/resources/notification.proto
-  npx pbts -o sw/src/pb.d.ts sw/src/pb.js
+    ../proto/isuxportal/resources/notification.proto"
+  npx -p protobufjs -c "pbts -o sw/src/pb.d.ts sw/src/pb.js"
 
 )
