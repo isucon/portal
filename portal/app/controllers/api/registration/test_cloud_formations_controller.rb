e776bc69a09ec447ffb240a4f835ca082bcee958
sapphi-red <green@sapphi.red>
2021-07-26 12:12:30 +0900

    models/cloud_formationへの抽出

--- portal/app/controllers/api/registration/test_cloud_formations_controller.rb
+++ portal/app/controllers/api/registration/test_cloud_formations_controller.rb
@@ -1,19 +1,10 @@
 require 'isuxportal/services/registration/cloud_formation_pb'
-require 'erb'
 
 class Api::Registration::TestCloudFormationsController < Api::Registration::ApplicationController
-  @@erb = ERB.new(File.read(File.join(File.dirname(__FILE__), '../../../cf_templates/test.yaml.erb')))
-
   def show
     raise ActiveRecord::RecordNotFound unless current_contestant
 
-    zone_id = current_team.availability_zone
-    template =
-      unless zone_id.nil?
-        @@erb.result(binding)
-      else
-        ''
-      end
+    template = CloudFormation.test_template(current_team.availability_zone)
 
     render protobuf: Isuxportal::Proto::Services::Registration::GetCloudFormationResponse.new(
       template: template,
