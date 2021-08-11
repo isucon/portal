f15e77a45846071926bbe80f02f548fcc47f78ef
sapphi-red <green@sapphi.red>
2021-08-11 16:51:26 +0900

    チェッカー用の情報を返すAPI

--- portal/app/controllers/api/env_checks_controller.rb
+++ portal/app/controllers/api/env_checks_controller.rb
@@ -1,15 +1,11 @@
 class Api::EnvChecksController < Api::ApplicationController
   skip_before_action :require_staff_when_always_required
   skip_before_action :verify_authenticity_token
+  before_action :require_valid_checker_token
 
   def create
-    token = params[:token]
-    return render status: :bad_request, body: "request not have token" unless token
-    payload = CheckerToken.verify(token)
-    return render status: :unauthorized, body: "invalid token" unless payload
-
     @env_check = EnvCheck.new(
-      team_id: payload[:team_id],
+      team_id: @payload[:team_id],
       name: params[:name],
       ip_address: params[:ip_address],
       passed: params[:passed],
@@ -18,5 +14,24 @@ class Api::EnvChecksController < Api::ApplicationController
       raw_data: params[:raw_data],
     )
     @env_check.save!
+  end
+
+  def info
+    team = Team.find(@payload[:team_id])
+
+    ami_id = "__ami_id__"
+    az_id = team.availability_zone
+
+    render json: {
+      ami_id: ami_id,
+      az_id: az_id,
+    }
+  end
+
+  private def require_valid_checker_token
+    token = params[:token]
+    return render status: :bad_request, body: "request not have token" unless token
+    @payload = CheckerToken.verify(token)
+    return render status: :unauthorized, body: "invalid token" unless @payload
   end
 end
