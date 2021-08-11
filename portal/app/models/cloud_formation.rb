5b56d4a274bbffb24534d508888212e46ba36ebc
sapphi-red <green@sapphi.red>
2021-08-12 03:57:13 +0900

    testのCFのCheckerTokenのexpiryの設定

--- portal/app/models/cloud_formation.rb
+++ portal/app/models/cloud_formation.rb
@@ -9,7 +9,10 @@ module CloudFormation
 
   def self.test_template(team)
     zone_id = team.availability_zone
-    token = CheckerToken.create(team_id: team.id)
+    token = CheckerToken.create(
+      team_id: team.id,
+      expiry: Rails.application.config.x.contest.contest_start.to_i,
+    )
 
     unless zone_id.nil?
       template(TEST_ERB, binding)
