88afec8d11d75f205d1e81d3d6ae36bd7cc5fee0
takonomura <takonomura@users.noreply.github.com>
2021-08-12 19:08:26 +0900

    Limit retry count (15)

--- envcheck/run-isucon-env-checker.sh
+++ envcheck/run-isucon-env-checker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -x
 
-while true; do
+retried=0
+while [[ $retried -le 15 ]]; do
   sleep $(( RANDOM % 15 ))
   /usr/local/bin/isucon-env-checker boot && exit 0
+  retried=$(( retried + 1 ))
 done
