775356074cce71d05fd87909540021b3853c2f6f
sapphi-red <green@sapphi.red>
2021-08-16 11:17:16 +0900

    bypass tokenのチーム離脱対応

--- portal/app/controllers/api/registration/sessions_controller.rb
+++ portal/app/controllers/api/registration/sessions_controller.rb
@@ -79,11 +79,14 @@ class Api::Registration::SessionsController < Api::Registration::ApplicationCont
   pb :delete, Isuxportal::Proto::Services::Registration::DeleteRegistrationRequest
   def delete
     raise ActiveRecord::RecordNotFound unless current_contestant
-    raise Api::ApplicationController::Errors::Forbidden.new("You cannot withdraw after start") if Contest.contest_started?
-    raise Api::ApplicationController::Errors::Forbidden.new("You cannot withdraw at this moment") if Contest.registration_update_closed?
+    was_leader = current_contestant.leader?
+
+    if was_leader || !current_bypass_allowed?(:LEAVE_TEAM)
+      raise Api::ApplicationController::Errors::Forbidden.new("You cannot withdraw after start") if Contest.contest_started?
+      raise Api::ApplicationController::Errors::Forbidden.new("You cannot withdraw at this moment") if Contest.registration_update_closed?
+    end
 
     contestants = []
-    was_leader = current_contestant.leader?
     ApplicationRecord.transaction do
       if was_leader
         current_team.update_attributes!(
