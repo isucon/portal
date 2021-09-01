d5e1bad38a2b94c2879ee1e8e04e2fdfe71d3181
sapphi-red <green@sapphi.red>
2021-09-01 23:18:06 +0900

    admin画面でのstatusによる絞り込み

--- portal/app/javascript/admin/AdminApiClient.ts
+++ portal/app/javascript/admin/AdminApiClient.ts
@@ -62,14 +62,18 @@ export class AdminApiClient {
     return klass.decode(new Uint8Array(await resp.arrayBuffer()));
   }
 
-  public async listBenchmarkJobs(teamId?: number | null, incompleteOnly?: boolean, page?: number) {
+  public async listBenchmarkJobs(teamId?: number | null, status?: isuxportal.proto.resources.BenchmarkJob.Status, page?: number) {
     const klass = isuxportal.proto.services.admin.ListBenchmarkJobsResponse;
+    const query: Record<string, string> = {
+      teamId: teamId?.toString() ?? '',
+      status: status?.toString() ?? '',
+      page: page?.toString() ?? ''
+    }
+
     const resp = await this.request(
-      `${this.baseUrl}/api/admin/benchmark_jobs?team_id=${teamId ? teamId.toString() : ""}&incomplete_only=${
-        incompleteOnly ? "1" : "0"
-      }&page=${page}`,
+      `${this.baseUrl}/api/admin/benchmark_jobs`,
       "GET",
-      null,
+      query,
       null
     );
     return klass.decode(new Uint8Array(await resp.arrayBuffer()));
--- portal/app/javascript/admin/AdminBenchmarkJobList.tsx
+++ portal/app/javascript/admin/AdminBenchmarkJobList.tsx
@@ -17,7 +17,7 @@ import ReactPaginate from "react-paginate";
 
 type ListFilterProps = {
   teamId: string | null;
-  incompleteOnly: boolean;
+  status: string | null;
 };
 const ListFilter: React.FC<ListFilterProps> = (props: ListFilterProps) => {
   const [redirect, setRedirect] = React.useState<JSX.Element | null>(null);
@@ -33,7 +33,7 @@ const ListFilter: React.FC<ListFilterProps> = (props: ListFilterProps) => {
   const onSubmit = handleSubmit((data) => {
     const search = new URLSearchParams();
     search.set("team_id", data.teamId || "");
-    search.set("incomplete_only", data.incompleteOnly ? "1" : "0");
+    search.set("status", data.status ?? "");
     setRedirect(
       <Redirect
         push={true}
@@ -60,15 +60,20 @@ const ListFilter: React.FC<ListFilterProps> = (props: ListFilterProps) => {
               </div>
             </div>
             <div className="column is-3 field">
-              <label className="label" htmlFor="AdminBenchmarkJobListFilter-incompleteOnly">
-                Incomplete only
+              <label className="label" htmlFor="AdminBenchmarkJobListFilter-status">
+                Status
               </label>
               <div className="control">
-                <input
-                  type="checkbox"
-                  id="AdminBenchmarkJobListFilter-incompleteOnly"
-                  {...register("incompleteOnly")}
-                />
+                <div className="select" id="AdminBenchmarkJobListFilter-status">
+                  <select {...register("status")}>
+                    <option value={""}>-----</option>
+                    <option value={isuxportal.proto.resources.BenchmarkJob.Status.PENDING.toString()}>PENDING</option>
+                    <option value={isuxportal.proto.resources.BenchmarkJob.Status.RUNNING.toString()}>RUNNING</option>
+                    <option value={isuxportal.proto.resources.BenchmarkJob.Status.ERRORED.toString()}>ERRORED</option>
+                    <option value={isuxportal.proto.resources.BenchmarkJob.Status.CANCELLED.toString()}>CANCELLED</option>
+                    <option value={isuxportal.proto.resources.BenchmarkJob.Status.FINISHED.toString()}>FINISHED</option>
+                  </select>
+                </div>
               </div>
             </div>
             <div className="column is-3 field">
@@ -87,7 +92,7 @@ export interface Props {
   session: isuxportal.proto.services.common.GetCurrentSessionResponse;
   client: AdminApiClient;
   teamId: string | null;
-  incompleteOnly: boolean;
+  status: isuxportal.proto.resources.BenchmarkJob.Status | null;
 }
 
 export interface State {
@@ -121,7 +126,7 @@ export class AdminBenchmarkJobList extends React.Component<Props, State> {
     try {
       const list = await this.props.client.listBenchmarkJobs(
         this.props.teamId ? parseInt(this.props.teamId, 10) : null,
-        this.props.incompleteOnly,
+        this.props.status ?? undefined,
         page
       );
       const pageCount = list.maxPage as number;
@@ -162,7 +167,7 @@ export class AdminBenchmarkJobList extends React.Component<Props, State> {
   }
 
   renderFilter() {
-    return <ListFilter teamId={this.props.teamId} incompleteOnly={this.props.incompleteOnly} />;
+    return <ListFilter teamId={this.props.teamId} status={this.props.status?.toString() ?? null} />;
   }
 
   renderPaginate() {
