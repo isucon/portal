d9b763d996708ffb47c8f3e35aed50203ce6a13e
Sorah Fukumori <her@sorah.jp>
2022-07-18 10:09:59 +0900

    add index for Contest.leaderboard(history: false)

--- portal/db/schema.rb
+++ portal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_221210) do
+ActiveRecord::Schema.define(version: 2022_07_17_234715) do
 
   create_table "benchmark_executions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -50,7 +50,9 @@ ActiveRecord::Schema.define(version: 2022_07_13_221210) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["benchmark_job_id"], name: "index_benchmark_results_on_benchmark_job_id", unique: true
     t.index ["finished", "exit_status", "exit_signal", "marked_at", "team_id"], name: "idx_leaderboard"
+    t.index ["finished", "exit_status", "exit_signal", "team_id", "marked_at", "id"], name: "idx_leaderboard2"
     t.index ["passed"], name: "index_benchmark_results_on_passed"
+    t.index ["team_id", "updated_at"], name: "index_benchmark_results_on_team_id_and_updated_at"
     t.index ["team_id"], name: "index_benchmark_results_on_team_id"
     t.index ["updated_at"], name: "index_benchmark_results_on_updated_at"
   end
