107589393b22dedc0a03c5481068c66f15c03dbc
Sorah Fukumori <her@sorah.jp>
2022-07-20 05:24:06 +0900

    MaintainBestBenchmarkResultOfTeamJob: limit 1

--- portal/app/jobs/maintain_best_benchmark_result_of_team_job.rb
+++ portal/app/jobs/maintain_best_benchmark_result_of_team_job.rb
@@ -2,9 +2,9 @@ class MaintainBestBenchmarkResultOfTeamJob < ApplicationJob
   def perform(team:, now: nil)
     ApplicationRecord.transaction do
       team.update!(
-        best_benchmark_result_id: scoped_benchmark_results(team: team, now: now, visibility: :contestant).order(score: :desc).pluck(:id)&.first,
-        best_benchmark_result_for_audience_id: scoped_benchmark_results(team: team, now: now, visibility: :audience).order(score: :desc).pluck(:id)&.first,
-        best_benchmark_result_for_admin_id: scoped_benchmark_results(team: team, now: now, visibility: :admin).order(score: :desc).pluck(:id)&.first,
+        best_benchmark_result_id: scoped_benchmark_results(team: team, now: now, visibility: :contestant).order(score: :desc).limit(1).pluck(:id)&.first,
+        best_benchmark_result_for_audience_id: scoped_benchmark_results(team: team, now: now, visibility: :audience).order(score: :desc).limit(1).pluck(:id)&.first,
+        best_benchmark_result_for_admin_id: scoped_benchmark_results(team: team, now: now, visibility: :admin).order(score: :desc).limit(1).pluck(:id)&.first,
       )
     end
   end
