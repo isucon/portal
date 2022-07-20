e90e92231608be068d9c70108af07a8a2ab0d7ff
Sorah Fukumori <her@sorah.jp>
2022-07-20 16:31:35 +0900

    admin/dashboards#show: expect cache_sum

--- portal/app/controllers/api/admin/dashboards_controller.rb
+++ portal/app/controllers/api/admin/dashboards_controller.rb
@@ -8,9 +8,9 @@ class Api::Admin::DashboardsController < Api::Admin::ApplicationController
 
     final = Rails.application.config.x.contest.final ? "final" : "qualify"
     clar_count = Clarification.unanswered.requested.count
-    cached_lb = Rails.cache.read("leaderboard-v2:#{final}:admin")&.yield_self { |_| Isuxportal::Proto::Resources::Leaderboard.decode(_) }
+    cache_sum, cache = Rails.cache.read("leaderboard-v2:#{final}:admin")
     render protobuf: Isuxportal::Proto::Services::Admin::DashboardResponse.new(
-      leaderboard: cached_lb || Contest.leaderboard(admin: true, team: nil, history: false),
+      leaderboard: cache_sum && cache ? Isuxportal::Proto::Resources::Leaderboard.decode(_) : Contest.leaderboard(admin: true, team: nil, history: false),
       unanswered_clarification_count: clar_count,
       earliest_unanswered_clarification_at: Clarification.unanswered.requested.order(created_at: :desc).limit(1).pluck(:created_at)&.first&.to_time,
     )
